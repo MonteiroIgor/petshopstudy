59ee15cf05855a4b5ffc3720ca45aa3c2ef37689
Igor <contato.igormonteiro@gmail.com>
2022-04-16 13:23:30 -0300

    Create Project

---
+++ src/main/java/com/example/petshopstudy/PetshopstudyApplication.java
@@ -0,0 +1,13 @@
+package com.example.petshopstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetshopstudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetshopstudyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/petshopstudy/PetshopstudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.petshopstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetshopstudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
