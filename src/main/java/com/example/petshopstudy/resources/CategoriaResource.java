7f3ba8fca23f87ce65f15d296677583684e123ae
Igor <contato.igormonteiro@gmail.com>
2022-04-16 14:21:11 -0300

    Update Controller and New Domain

--- src/main/java/com/example/petshopstudy/resources/CategoriaResource.java
+++ src/main/java/com/example/petshopstudy/resources/CategoriaResource.java
@@ -1,15 +1,28 @@
 package com.example.petshopstudy.resources;
 
+import com.example.petshopstudy.domain.Categoria;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping(value= "/categorias")
 public class CategoriaResource {
 
     @RequestMapping(method = RequestMethod.GET)
-    public String mostrar() {
-        return "Test Rest";
+    public List<Categoria> mostrar() {
+        Categoria cat1 = new Categoria(1, "Alimento");
+        Categoria cat2 = new Categoria(2, "Cosmético");
+        Categoria cat3 = new Categoria(3, "Remédio");
+
+        List<Categoria> listCategorias = new ArrayList<>();
+        listCategorias.add(cat1);
+        listCategorias.add(cat2);
+        listCategorias.add(cat3);
+
+        return listCategorias;
     }
 }
